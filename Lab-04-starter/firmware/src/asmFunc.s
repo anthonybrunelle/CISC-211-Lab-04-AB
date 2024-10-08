ba14b42fa4aeeb840e577880e9d0648f32f97de2
Anthony <anthorun@gmail.com>
2024-10-07 17:46:56 -0700

    Completed lab

--- Lab-04-starter/firmware/src/asmFunc.s
+++ Lab-04-starter/firmware/src/asmFunc.s
@@ -67,6 +67,73 @@ asmFunc:
     
     /*** STUDENTS: Place your code BELOW this line!!! **************/
 
+    /* Set output variables to 0 */
+    LDR r1, =eat_out /* Load address of eat_out into r1 */
+    MOVS r2, 0 /* Move 0 into r2 */
+    STR r2, [r1] /* Store value of r2 into address of eat_out */
+    
+    LDR r1, =stay_in /* Load address of stay_in into r1 */
+    STR r2, [r1] /* Store value of r2 (0 from before) into address of stay_in */
+    
+    LDR r1, =eat_ice_cream /* Load address of eat_ice_cream into r1 */
+    STR r2, [r1] /* Store 0 into address of eat_ice_cream */
+    
+    LDR r1, =we_have_a_problem /* Load address of we_have_a_problem into r1 */
+    STR r2, [r1] /* Store 0 into address of we_have_a_problem */
+    
+    /* Load balance and add transaction amount */
+    LDR r1, =balance /* Load address of balance into r1 */
+    LDR r3, [r1] /* Load value at address of r1 (balance) into r3 */
+    ADDS r4, r3, r0 /* Store sum of balance and transaction amount (r0) into r4 */
+    BVS problem /* If V flag set branch to problem */
+    
+    /* Check if transaction is out of range */
+    CMP r0, 1000 /* Compare transaction (r0) to 1000 */
+    BGT problem /* Branch to problem IF transaction > 1000 */
+    CMP r0, -1000 /* Compare transaction (r0) to 1000 */
+    BLT problem /* Branch to problem IF transaction < -1000 */
+    
+    /* Update balance and transaction */
+    STR r4, [r1] /* Store tmp_balance (r4) into balance (r1) */
+    LDR r1, =transaction /* Load address of transaction into r1 */
+    STR r0, [r1] /* Store transaction amount (r0) into transaction */
+    
+    /* Check new balance */
+    MOV r0, r4 /* Move tmp_balance (r4) into r0 */
+    CMP r4, 0 /* Compare tmp_balance with 0 */
+    BGT we_eat_out /* Branch to we_eat_out IF tmp_balance > 0 */
+    BLT we_stay_in /* Branch to we_stay_in IF tmp_balance < 0 */
+    
+    LDR r1, =eat_ice_cream /* Load address of eat_ice_cream into r1 */
+    MOVS r2, 1 /* Move 1 into r2 */
+    STR r2, [r1] /* Store 1 (r2) into address of eat_ice_cream (r1) */
+    B done /* Branch to done */
+    
+problem:
+    /* Handle out of bounds transactions (-1000 to 1000) */
+    LDR r1, =we_have_a_problem /* Load address of we_have_a_problem into r1 */
+    MOVS r2, 1 /* Move 1 into r2 */
+    STR r2, [r1] /* Store 1 (r2) into address of we_have_a_problem (r1) */
+    
+    /* Reset transaction to 0 */
+    LDR r1, =transaction /* Load address of transaction into r1 */
+    MOVS r2, 0 /* Move 0 into r2 */
+    STR r2, [r1] /* Store 0 (r2) into address of transaction (r1) */
+    
+    MOV r0, r3 /* Move balance (r3) into r0 */
+    B done /* Branch to done */
+
+we_eat_out:
+    LDR r1, =eat_out /* Load address of eat_out into r1 */
+    MOVS r2, 1 /* Move 1 into r2 */
+    STR r2, [r1] /* Store 1 (r2) into address of eat_out (r1) */
+    B done /* Branch to done */
+
+we_stay_in:
+    LDR r1, =stay_in /* Load address of stay_in into r1 */
+    MOVS r2, 1 /* Move 1 into r2 */
+    STR r2, [r1] /* Store 1 (r2) into address of stay_in (r1) */
+    B done /* Branch to done */
     
     /*** STUDENTS: Place your code ABOVE this line!!! **************/
 
